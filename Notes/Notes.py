5801770f7c633e164fd8f3efc89369f57d406d66
kea <kavchenkov@gmail.com>
2022-03-29 18:43:39 +0200

    Создал блокнот (нотес) для написания всего подряд. Создал лессон_2 в котором прохожу урок на степике.

---
+++ Notes/Notes.py
@@ -0,0 +1,10 @@
+print('Мы изучаем язык Python')
+print("Мы изучаем язык Python")
+
+a = 'a'
+a1 = "a"
+
+print(a == a1)
+print(f"Some lorient text {a=}. Don't")
+for i in range(0, 10):
+    print(f'{i=}')
