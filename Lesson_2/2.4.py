c732055d6f12abc2f2d246696e6671f47fb32271
kea <kavchenkov@gmail.com>
2022-03-30 18:21:05 +0200

    Пройден урок 2.3 и начал разбирать урок 2.4

---
+++ Lesson_2/2.4.py
@@ -0,0 +1,16 @@
+num1 = 7            # num1 - это число
+num2 = 10           # num2 - это число
+num3 = num1 + num2  # num3 - это число
+print(num3, end='\n\n')
+
+a = 3
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b, end='\n\n')
+
+num1 = 2 + 3 * 4
+num2 = (2 + 3) * 4
+print(num1)
+print(num2)
