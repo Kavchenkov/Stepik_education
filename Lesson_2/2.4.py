a014c44d6e4e0dc8d44e400deaae29e2cb758d26
kea <kavchenkov@gmail.com>
2022-03-30 22:33:39 +0200

    Успешно завершил урок 2.4 Все рабочие коды из степика копировал в 2.4

--- Lesson_2/2.4.py
+++ Lesson_2/2.4.py
@@ -14,3 +14,64 @@ num1 = 2 + 3 * 4
 num2 = (2 + 3) * 4
 print(num1)
 print(num2)
+
+s = '1992'
+year = int(s)
+print(year)
+
+num1 = input()
+num2 = input()
+print(num1 + num2)
+
+num1 = int(input())
+num2 = int(input())
+print(num1 + num2)
+
+num = 17
+s = str(17)
+print(s)
+
+num1 = -6      # унарный минус
+print(num1)
+num2 = 17 - 7  # бинарный минус
+print(num2)
+
+x = int(input())  # первый вариант вывода на печать
+y = x + 1
+z = y + 1
+print(x)
+print(y)
+print(z)
+
+x = int(input())  # второй вариант вывода на печать
+y = x + 1
+z = y + 1
+print(x, y, z, sep = "\n")
+
+x, y, z = int(input()), int(input()), int(input())
+print(x+y+z)
+
+x = int(input())
+print('Объем =', x**3)
+print('Площадь полной поверхности =', 6*x**2)
+
+a, b = int(input()), int(input())
+print(3*(a+b)**3+275*b**2-127*a-41)
+
+x = int(input())
+print('Следующее за числом', x, 'число:', x+1)
+print('Для числа', x, 'предыдущее число:', x-1)
+
+x, y, z, f = int(input()), int(input()), int(input()), int(input())
+print((x+y+z+f)*3)
+
+x, y = int(input()), int(input())
+print(x, '+', y, '=', x+y)
+print(x, '-', y, '=', x-y)
+print(x, '*', y, '=', x*y)
+
+x, y, z = int(input()), int(input()), int(input())
+print(x+y*(z-1))
+
+x = int(input())
+print(x, 2*x, 3*x, 4*x, 5*x, sep='---')
--- Notes/Notes.py
+++ Notes/Notes.py
@@ -9,9 +9,22 @@ print(f"Some lorient text {a=}. Don't")
 for i in range(0, 10):
     print(f'{i=}')
 
-num1 = print(input())
-print(num3)
-num2 = print(input())
-print(num2)
-num3 = print(input())
-print(num1)
+name1 = input()
+name2 = input()
+name3 = input()
+
+print(name3)
+print(name2)
+print(name1)
+
+x = int(input())
+y = x + 1
+z = y + 1
+print(x)
+print(y)
+print(z)
+
+x = int(input())
+y = x + 1
+z = y + 1
+print(x, y, z, sep = "\n")
