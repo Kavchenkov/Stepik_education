c4519002007b346f30903e323362662a1c3f1426
kea <kavchenkov@gmail.com>
2022-04-06 20:39:52 +0200

    Пройден 5 урок (экзамен) (22%).

--- Lesson_5/5.py
+++ Lesson_5/5.py
@@ -47,6 +47,6 @@ x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
 if x2 != x1 and y2 != y1 and (x2 - y2 == x1 - y1 or x2 + y2 == x1 + y1) or x1 == x2 or y1 == y2:
     print('YES')
 else:
-    print('NO')                                                                                  
+    print('NO')
 
 
